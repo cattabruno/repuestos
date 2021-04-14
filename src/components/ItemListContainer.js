f25621070ce5b4bf791168df9dbb6f338da5c5e0
Bruno Cattaneo <cattabruno@gmail.com>
2021-04-13 22:28:52 -0300

    ItemListContainer

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,11 +1,10 @@
 import React from 'react';
 
-
 export default function ItemListContainer(props) {
     return(
         <div className="header">
             <p>{props.greeting}</p>
-            <p>Todas las marcas <a className="header-portybox" href="#">{props.name}</a>! Piezas Originales.</p>
+            <p>Todas las marcas{props.name}! Piezas Originales.</p>
         </div>
     )
 }
