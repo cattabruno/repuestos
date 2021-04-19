84b292b7f2758500cff2e22922a18cf13daedc89
Bruno Cattaneo <cattabruno@gmail.com>
2021-04-19 17:55:22 -0300

    Stock agregado

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -1,39 +1,53 @@
 import React, { useState } from "react";
 
 class ItemCount extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        count: 0
-      };
-      
-      this.increment = this.increment.bind(this);
-      this.decrement = this.decrement.bind(this);
-    }
-    
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
+  }
+
   increment() {
-  
-      this.setState(prevState=>({
-    count:prevState.count +1
-    }));
-  }
-    
-    decrement() {
-  
-      this.setState(prevState=>({
-    count:prevState.count -1
-    }));
-  }
-    
-    render() {
-      return (
-        <div>
-          <button className='inc' onClick={this.increment}>Agregar</button>
-          <button className='dec' onClick={this.decrement}>Quitar</button>
-          <h1>Contador: {this.state.count}</h1>
-        </div>
-      );
+    if (this.state.count >= 10) {
+      this.setState({
+        count: 10,
+      });
+    } else {
+      this.setState({
+        count: this.state.count + 1,
+      });
     }
-  };
+  }
+
+  decrement() {
+    if (this.state.count <= 0) {
+      this.setState({
+        count: 0,
+      });
+    } else {
+      this.setState({
+        count: this.state.count - 1,
+      });
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <button className="inc" onClick={this.increment}>
+          Agregar
+        </button>
+        <button className="dec" onClick={this.decrement}>
+          Quitar
+        </button>
+        <h1>Contador: {this.state.count}</h1>
+      </div>
+    );
+  }
+}
 
 export default ItemCount;
